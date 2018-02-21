5b9fb6947d4f7a3ad4c55a6bb3d951394e58cf60
TotBrigi <rige07@gmail.com>
2018-02-21 19:07:40 +0100

    while loop, def main(), life, score, random.randint()

--- Guess-the-secret-number.py
+++ Guess-the-secret-number.py
@@ -1,9 +1,36 @@
-secret = 117
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import random
 
-guess = int(raw_input("Guess the secret number!"))
+def main():
+    secret = random.randint(0,20)
+    life = 3
+    score = 0
+    new_game = "yes"
 
-if (secret == guess):
-    print "Wow, you are awsome!!! This is really the " + str(secret)
-else:
-    print "Maybe next time! :( This is not " + str(guess)
+    while life>0 and new_game == "yes":
+        guess = int(raw_input("Guess the secret number between 0 and 20!"))
 
+        if (secret == guess):
+            print "Wow, you are awsome!!! This is really the " + str(secret)
+        elif (secret > guess):
+            print "It is bigger!"
+        elif (secret < guess):
+            print "It is smaller!"
+        if (secret != guess):
+            life -= 1
+            score -= 1
+            print "Life: " + str(life)
+            print "Score: " + str(score)
+        else:
+            life += 1
+            score +=1
+            print "Life: " + str(life)
+            print "Score: " + str(score)
+        if life == 0:
+            print "This was my secret number: " + str(secret)
+            print "Good bye!"
+
+
+if __name__ == "__main__":
+    main()
